a06bd68b4d2310167dc05256071e82a89aac8fb2
Michal Kváček <michal.kvacek@monstermedia.cz>
2018-05-30 15:36:10 +0200

    FIX: eshop reviews

--- src/EshopReview.php
+++ src/EshopReview.php
@@ -5,116 +5,92 @@ namespace OndraKoupil\Heureka;
 /**
  * Recenze e-shopu
  */
-class EshopReview {
+class EshopReview
+{
+    /**
+     * Číslo recenze v rámci souboru, začíná se od 0
+     * @var number
+     */
+    public $index;
+    /**
+     * Jedinečné ID recenze (dané Heurékou)
+     * @var number
+     */
+    public $ratingId;
+    /**
+     * Jméno autora. Empty string = anonymní.
+     * @var string
+     */
+    public $author;
+    /**
+     * @var bool
+     */
+    public $recommends;
+    /**
+     * Datum a čas napsání recenze
+     * @var \DateTime
+     */
+    public $date;
+    /**
+     * Hodnocení celkové - na stupnici 0.5 až 5 hvězdiček
+     * @var number|null
+     */
+    public $ratingTotal;
+    /**
+     * Hodnocení délky dodací lhůty - na stupnici 0.5 až 5 hvězdiček
+     * @var number|null
+     */
+    public $ratingDelivery;
+    /**
+     * Hodnocení kvality dopravy zboží - na stupnici 0.5 až 5 hvězdiček
+     * @var number|null
+     */
+    public $ratingTransportQuality;
+    /**
+     * Hodnocení použitelnosti a přehlednosti e-shopu
+     * na stupnici 0.5 až 5 hvězdiček
+     * @var number|null
+     */
+    public $ratingWebUsability;
+    /**
+     * Hodnocení komunikace ze strany e-shopu
+     * na stupnici 0.5 až 5 hvězdiček
+     * @var number|null
+     */
+    public $ratingCommunication;
+    /**
+     * Hlavní výhody e-shopu.
+     * Víceřádkový řetězec, zpravidla co řádek, to jeden bod
+     * @var string
+     */
+    public $pros;
+    /**
+     * Hlavní nevýhody e-shopu.
+     * Víceřádkový řetězec, zpravidla co řádek, to jeden bod
+     * @var string
+     */
+    public $cons;
+    /**
+     * Celkové shrnutí názoru zákazníka na obchod
+     * @var string
+     */
+    public $summary;
+    /**
+     * Reakce provozovatele e-shopu na recenzi zákazníka
+     * @var string
+     */
+    public $reaction;
+    /**
+     * Číslo objednávky, na níž zákazník psal recenzi
+     * @var string
+     */
+    public $orderId;
 
-	/**
-	 * Číslo recenze v rámci souboru, začíná se od 0
-	 *
-	 * @var number
-	 */
-	public $index;
-
-	/**
-	 * Jedinečné ID recenze (dané Heurékou)
-	 *
-	 * @var number
-	 */
-	public $ratingId;
-
-	/**
-	 * Jméno autora. Empty string = anonymní.
-	 *
-	 * @var string
-	 */
-	public $author;
-
-	/**
-	 * Datum a čas napsání recenze
-	 *
-	 * @var \DateTime
-	 */
-	public $date;
-
-	/**
-	 * Hodnocení celkové - na stupnici 0.5 až 5 hvězdiček
-	 *
-	 * @var number|null
-	 */
-	public $ratingTotal;
-
-	/**
-	 * Hodnocení délky dodací lhůty - na stupnici 0.5 až 5 hvězdiček
-	 *
-	 * @var number|null
-	 */
-	public $ratingDelivery;
-
-	/**
-	 * Hodnocení kvality dopravy zboží - na stupnici 0.5 až 5 hvězdiček
-	 *
-	 * @var number|null
-	 */
-	public $ratingTransportQuality;
-
-	/**
-	 * Hodnocení použitelnosti a přehlednosti e-shopu
-	 * na stupnici 0.5 až 5 hvězdiček
-	 *
-	 * @var number|null
-	 */
-	public $ratingWebUsability;
-
-	/**
-	 * Hodnocení komunikace ze strany e-shopu
-	 * na stupnici 0.5 až 5 hvězdiček
-	 *
-	 * @var number|null
-	 */
-	public $ratingCommunication;
-
-	/**
-	 * Hlavní výhody e-shopu.
-	 * Víceřádkový řetězec, zpravidla co řádek, to jeden bod
-	 *
-	 * @var string
-	 */
-	public $pros;
-
-	/**
-	 * Hlavní nevýhody e-shopu.
-	 * Víceřádkový řetězec, zpravidla co řádek, to jeden bod
-	 *
-	 * @var string
-	 */
-	public $cons;
-
-	/**
-	 * Celkové shrnutí názoru zákazníka na obchod
-	 *
-	 * @var string
-	 */
-	public $summary;
-
-
-	/**
-	 * Reakce provozovatele e-shopu na recenzi zákazníka
-	 *
-	 * @var string
-	 */
-	public $reaction;
-
-	/**
-	 * Číslo objednávky, na níž zákazník psal recenzi
-	 *
-	 * @var string
-	 */
-	public $orderId;
-
-	/**
-	 * @return array
-	 */
-	function getAsArray() {
-		return get_object_vars($this);
-	}
-
+    /**
+     * @return array
+     */
+    function getAsArray()
+    {
+        return get_object_vars($this);
+    }
 }
--- src/EshopReviewsClient.php
+++ src/EshopReviewsClient.php
@@ -5,77 +5,74 @@ namespace OndraKoupil\Heureka;
 /**
  * Klient umožňující stahovat recenze e-shopu jako takového
  */
-class EshopReviewsClient extends BaseClient {
+class EshopReviewsClient extends BaseClient
+{
+    function getNodeName()
+    {
+        return "review";
+    }
 
-	function getNodeName() {
-		return "review";
-	}
+    public function setKey($key)
+    {
 
-	public function setKey($key) {
+        if (preg_match('~^\w{32}$~', $key)) {
+            $this->setSourceAddress("https://www.heureka.cz/direct/dotaznik/export-review.php?key=" . $key);
+        } else {
+            $this->setSourceAddress($key);
+        }
+    }
 
-		if (preg_match('~^\w{32}$~', $key)) {
-			$this->setSourceAddress("https://www.heureka.cz/direct/dotaznik/export-review.php?key=" . $key);
-		} else {
-			$this->setSourceAddress($key);
-		}
+    /**
+     * @ignore
+     */
+    public function processElement(\SimpleXMLElement $element, $index)
+    {
 
-	}
+        $review = new EshopReview();
 
-	/**
-	 * @ignore
-	 */
-	public function processElement(\SimpleXMLElement $element, $index) {
+        $review->index = $index;
+        $review->author = (string)$element->name;
+        $review->cons = (string)$element->cons;
+        $review->date = new \DateTime();
+        $review->date->setTimestamp((int)$element->unix_timestamp);
+        $review->orderId = (string)$element->order_id;
+        $review->pros = (string)$element->pros;
+        $review->recommends = boolval($element->recommends->__toString());
 
-		$review = new EshopReview();
+        if (count($element->communication)) {
+            $review->ratingCommunication = (float)$element->communication;
+        } else {
+            $review->ratingCommunication = null;
+        }
 
-		$review->index = $index;
-		$review->author = (string)$element->name;
-		$review->cons = (string)$element->cons;
-		$review->date = new \DateTime();
-		$review->date->setTimestamp((int)$element->unix_timestamp);
-		$review->orderId = (string)$element->order_id;
-		$review->pros = (string)$element->pros;
+        if (count($element->delivery_time)) {
+            $review->ratingDelivery = (float)$element->delivery_time;
+        } else {
+            $review->ratingDelivery = null;
+        }
 
-		if (count($element->communication)) {
-			$review->ratingCommunication = (float)$element->communication;
-		} else {
-			$review->ratingCommunication = null;
-		}
+        if (count($element->total_rating)) {
+            $review->ratingTotal = (float)$element->total_rating;
+        } else {
+            $review->ratingTotal = null;
+        }
 
-		if (count($element->delivery_time)) {
-			$review->ratingDelivery = (float)$element->delivery_time;
-		} else {
-			$review->ratingDelivery = null;
-		}
+        if (count($element->transport_quality)) {
+            $review->ratingTransportQuality = (float)$element->transport_quality;
+        } else {
+            $review->ratingTransportQuality = null;
+        }
 
-		if (count($element->total_rating)) {
-			$review->ratingTotal = (float)$element->total_rating;
-		} else {
-			$review->ratingTotal = null;
-		}
+        if (count($element->web_usability)) {
+            $review->ratingWebUsability = (float)$element->web_usability;
+        } else {
+            $review->ratingWebUsability = null;
+        }
 
-		if (count($element->transport_quality)) {
-			$review->ratingTransportQuality = (float)$element->transport_quality;
-		} else {
-			$review->ratingTransportQuality = null;
-		}
+        $review->ratingId = (int)$element->rating_id;
+        $review->reaction = (string)$element->reaction;
+        $review->summary = (string)$element->summary;
 
-		if (count($element->web_usability)) {
-			$review->ratingWebUsability = (float)$element->web_usability;
-		} else {
-			$review->ratingWebUsability = null;
-		}
-
-		$review->ratingId = (int)$element->rating_id;
-		$review->reaction = (string)$element->reaction;
-		$review->summary = (string)$element->summary;
-
-		return $review;
-
-	}
-
-
-
-
-
+        return $review;
+    }
 }
